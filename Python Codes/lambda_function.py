4089bae934ec061e383c3556bcd95a2d1de436b7
ohmic-guy <omkarankit2004@gmail.com>
2025-07-28 15:20:27 +0530

    lambda function initialization

--- Python Codes/lambda_function.py
+++ Python Codes/lambda_function.py
@@ -1,5 +1,11 @@
-# print((lambda x: x**2)(6))
+print((lambda x: x**2)(6))
 
-# lambda_cube= (lambda y: y*y*y)
-# print(lambda_cube(5))
+lambda_cube= (lambda y: y*y*y)
+print(lambda_cube(5))
 
+list1= [1,2,3,4,5,6,7,8,9]
+filter(lambda x: x%2==0,list1)
+print(list(filter(lambda x: x%2==0,list1)))
+
+list2= [2,3,4,5,6,7,8]
+print(list(map(lambda x: pow(x,2), list2)))
