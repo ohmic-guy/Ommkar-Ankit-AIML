a5d95302dbe6a2ff1e726ac85957caf67f9720e5
ohmic-guy <omkarankit2004@gmail.com>
2025-09-16 23:08:44 +0530

    excluded .class files

--- Python Codes/wine_quality/wine.py
+++ Python Codes/wine_quality/wine.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import numpy as np
 
 
 rw = pd.read_csv('winequality-red.csv', sep=';')
